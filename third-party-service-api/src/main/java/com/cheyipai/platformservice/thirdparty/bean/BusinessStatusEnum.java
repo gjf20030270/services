bdd0b3d7ac0aa0ebe6bbd8b678be4460cefb6178
gongjunfeng <gongjunfeng@cheyipai.com>
2017-03-22 13:59:46 +0800

    代码调整

--- third-party-service-api/src/main/java/com/cheyipai/platformservice/thirdparty/bean/BusinessStatusEnum.java
+++ third-party-service-api/src/main/java/com/cheyipai/platformservice/thirdparty/bean/BusinessStatusEnum.java
@@ -11,13 +11,13 @@ package com.cheyipai.platformservice.thirdparty.bean;
 public enum BusinessStatusEnum {
 
     //成功与失败的key-value , key长度为1
-    SUCCESS("TH00001", "操作成功"),
-    FAIL("TH0009", "发生错误"),
+    SUCCESS("1", "操作成功"),
+    FAIL("0", "发生错误"),
 
 
-    PARAM_EMPTY("DDD", "输入参数为空"),
-    CAPTCHA_ERROR("DDDD", "验证码错误"),
-    SECURE_ERROR("1003", "安全校验错误");
+    PARAM_EMPTY("10001", "输入参数为空"),
+    CAPTCHA_ERROR("10002", "验证码错误"),
+    SECURE_ERROR("10003", "安全校验错误");
 
     private BusinessStatusEnum(String resultCode, String stateDescription) {
         this.resultCode = resultCode;
